54546b4f7a77beb5a74dfdf6c1fac2d916ef5fda
ergobot <stobryan@gmail.com>
2016-05-02 16:26:33 -0500

    Added light sensor

--- app/src/main/java/com/example/sensorslist/MainActivity.java
+++ app/src/main/java/com/example/sensorslist/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.sensorslist;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mLight;
+
+    TextView lightValueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        lightValueTextView = (TextView)findViewById(R.id.lightvalue);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float lux = event.values[0];
+        String lightValue = "Value: " + Float.toString(lux) + " (lx)";
+        // Do something with this sensor value.
+        lightValueTextView.setText(lightValue);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
